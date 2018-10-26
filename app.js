bb85bf65ef2cd658ab98e8160144327d9a23dc41
Danish Bansal <38431950+danishbansal808@users.noreply.github.com>
2018-10-26 16:03:09 +0530

    search fixed and angular added
    search fixed

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const fileupload = require('express-fileupload');
 const id = require('crypto-random-string');
 const date = require('date-and-time');
 /*---------------------------------------------------------------------------*/
-var app=express();
+var app = express();
 app.use(fileupload());
 var connection = mysql.createPool({
-  connectionLimit:30,
-  host:'127.0.0.1',
-  user:'root',
-  password:'',
-  database:'test3'
+  connectionLimit: 30,
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'test3'
 });
 var transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -22,111 +22,121 @@ var transporter = nodemailer.createTransport({
     pass: 'D@nish123'
   }
 });
-var urlencodedParser = bodyParser.urlencoded({ extended: false },
-  function(err, client) {
-    if (err) {
-      console.log("err");
-    }
-    console.log('connect!!!');
-  });
-  let now = new Date();
-date.format(now, 'YYYY/MM/DD');
-app.set('view engine','ejs');
-app.use('/assets',express.static('assets'));
-app.get('/',function(req,res){
+var urlencodedParser = bodyParser.urlencoded({
+  extended: false
+}, function(err, client) {
+  if (err) {
+    console.log("err");
+  }
+  console.log('connect!!!');
+});
+let now = new Date();
+date.format(now, 'YYYY/MM/DD HH:mm:ss');
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+app.get('/', function(req, res) {
   res.render('home');
 });
-app.get('/home',function(req,res){
+app.get('/home', function(req, res) {
   res.render('home');
 });
-app.get('/contact',function(req,res){
+app.get('/contact', function(req, res) {
   res.render('contact');
 });
-app.get('/complaints',function(req,res){
-  res.render('complaints',{qs: req.query});
+app.get('/complaints', function(req, res) {
+  res.render('complaints', {
+    qs: req.query
+  });
 });
-app.post('/complaints',urlencodedParser,function(req,res){
-  var name=req.body.uname,
-      email=req.body.email,
-      phone=req.body.pnumber,
-      block=req.body.block,
-      floor=req.body.floor,
-      room=req.body.room,
-      complaint=req.body.complainttype,
-      description=req.body.Description,
-      date=Date.now();
-      uid=id(8);
-  var stmnt=`INSERT INTO complaints(Name, Email, Contact, Block, Floor, Room, Complaints, Description, Date, ID) VALUES (?,?,?,?,?,?,?,?,?,?)`;
-  var alldata=[name,email,phone,block,floor,room,complaint,description,date,uid];
-  var mailOptions = {
-  from: 'bandabhadurhostel@gmail.com',
-  to: email,
-  subject: 'Query Recieved',
-  text: `Thanks For Contacting Us ! We will Reply you soon
-  your complaint is about ---> ${complaint}
-   and
-   description is-----> ${description}
-    Your unique id is----> ${uid}
-    please use it for future refrence`
-};
-
- connection.getConnection(function(err,tempcount){
-    if(!!err){
-      console.log("error");
-    }else{
-      transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-      console.log("connected");
-    }
-});
-connection.query(stmnt  , alldata ,function(err,row,fields){
-  if(!!err){
-    console.log(err);
-  }else{
-    console.log("Data inserted successfully");
-    res.render('complaints-success',{data: req.body,uid});
-  }
-})
-});
-app.get('/news',function(req,res){
+app.get('/news', function(req, res) {
   res.render('news');
 });
-app.get('/admin',function(req,res){
+app.get('/admin', function(req, res) {
   res.render('adminpage');
 });
-app.get('/admin/edithomepage',function(req,res){
+app.get('/edithomepage', function(req, res) {
   res.render('edithomepage');
 });
+app.get('/managecomplaints', function(req, res) {
+  res.render('managecomplaints');
+});
+app.post('/complaints', urlencodedParser, function(req, res) {
+  var name = req.body.uname,
+    email = req.body.email,
+    phone = req.body.pnumber,
+    block = req.body.block,
+    floor = req.body.floor,
+    room = req.body.room,
+    complaint = req.body.complainttype,
+    description = req.body.Description,
+    date = now;
+  uid = id(8);
+  var stmnt = `INSERT INTO complaints(Name, Email,
+Contact, Block, Floor, Room, Complaints, Description, Date, ID) VALUES (?,?,?,?,?,?,?,?,?,?)`;
+  var alldata = [name, email, phone, block, floor, room, complaint, description, date, uid];
+  var mailOptions = {
+    from: 'bandabhadurhostel@gmail.com',
+    to: email,
+    subject: 'Query Recieved',
+    text: `Thanks For Contacting Us ! We will Reply you soon
+    your complaint is about ---> ${complaint}
+    and
+    description is-----> ${description}
+    Your unique id is----> ${uid}
+    Please use it for future refrence`
+  };
+  connection.getConnection(function(err, tempcount) {
+    if (!!err) {
+      console.log("error");
+    } else {
+      transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+      console.log("connected");
+    }
+  });
+  connection.query(stmnt, alldata, function(err, row, fields) {
+    if (!!err) {
+      console.log(err);
+    } else {
+      console.log("Data inserted successfully");
+      res.render('complaints-success', {
+        data: req.body,
+        uid
+      });
+    }
+  })
+});
+
 app.post('/upload', function(req, res) {
   if (Object.keys(req.files).length == 0) {
     return res.status(400).send('No files were uploaded.');
   }
   let sampleFile = req.files.sampleFile;
   sampleFile.mv('assets/.jpg', function(err) {
-    if (err)
-      return res.status(500).send(err);
-
+    if (err) return res.status(500).send(err);
     res.send('File uploaded!');
   });
 });
-app.post('/searchdata',urlencodedParser,function(req,res){
-  var id=req.body.uid;
+app.post('/searchdata', urlencodedParser, function(req, res) {
+  var id = req.body.uid;
   console.log(id);
   console.log(now);
-var sql = 'SELECT * FROM complaints WHERE ID = ?';
-connection.getConnection(function(err){
-  if (err) throw err;
-  console.log('connect');
-connection.query(sql, id,function(err,result){
-  if (err) throw err;
-  res.send(result);
-  console.log(result);
-});
-});
+  var sql = 'SELECT * FROM `complaints` WHERE `ID` = ?';
+  connection.getConnection(function(err) {
+    if (err) throw err;
+    console.log('connect');
+    connection.query(sql, id, function(err, row,fields) {
+      if (err) throw err;
+    //  res.render('yourcomplaint',{result:result});
+      res.send(row[0]);
+      console.log(row[1]);
+    });
+  });
 });
 app.listen(3000);
+console.log(now);
